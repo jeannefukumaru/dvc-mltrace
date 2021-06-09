dce1c54b23994403262c84e7bfed3cd105d73b0d
jeanne <jiawenchoo@gmail.com>
2021-06-09 16:23:59 +0800

    refactor code by adding mltrace decorators and putting code into functions

--- 01_clean_data.py
+++ 01_clean_data.py
@@ -1,12 +1,22 @@
 import pandas as pd 
+from mltrace import register, create_component
 
-raw = pd.read_csv('data/abalone.data', header=None)
+@register(component_name="clean", input_vars=['input_filepath'], output_vars=['output_filepath'])
+def clean(input_filepath: str, output_filepath: str) -> str:
+    """
+    process raw data by adding column names
+    """
+    raw = pd.read_csv(input_filepath, header=None)
+    print('adding column names')
+    raw.columns = ['sex','length','diameter','height','whole_weight','shucked_weight','viscera_weight','shell_weight','rings']
+    print('saving data to csv')
+    raw.to_csv(output_filepath, index=False)
 
-print('adding column names')
-
-raw.columns = ['sex','length','diameter','height','whole_weight','shucked_weight','viscera_weight','shell_weight','rings']
-
-print('saving data to csv')
-
-raw.to_csv('data/abalone.csv')
+if __name__=="__main__":
+    create_component(
+        name="cleaning",
+        description="process raw data by adding column names",
+        owner="jeanne",
+        tags=["etl"],)
+    clean('data/abalone.data','data/abalone.csv')
 
